c140fd0d70ab860ff5390f73993eef4c7a69404e
Augustine Xu <31711513+augustinexzy@users.noreply.github.com>
2019-11-21 17:01:17 -0700

    1

--- new/1.plot.py
+++ new/1.plot.py
@@ -27,6 +27,7 @@ def first_plot(df):
     year = df['Incident Begin Date'].str.split('-', n=1, expand=True)
     df['year'] = year[0]
     df.drop(columns = ['Incident Begin Date'], inplace= True)
+    df = pd.Series(df['year'])
     new = df.value_counts()
     new = new.sort_index()
     plt.figure(figsize=(14, 8), dpi=80)
@@ -36,14 +37,12 @@ def first_plot(df):
     plt.title('随时间变化事故出现的次数', fontsize=15)
     plt.xlabel('时间', fontsize=15)
     plt.ylabel('次数', fontsize=15)
-    for a, b in zip(date, value):
-        plt.text(a, b, b, ha='center', va='bottom', fontsize=15)
-
     plt.show()
 
 
 
 def second_plot(df):
+    df = pd.Series(df['State '])
     new = df.value_counts()
     new = new.sort_index()
     x = new.keys()
@@ -62,7 +61,6 @@ def main():
 	    	if '.csv' in file:
 	    		print (file)
 	    		df = pd.read_csv(file,sep=',',header=0, usecols=[10])
-    			date, value, datas = parse(df)
     			first_plot(date, value)
     # list = Counter([row['State'] for row in pd.read_csv('Flood.csv').iterrows()])
     			data = pd.read_csv(file,sep=',',header=0, usecols=[5])
